dbc192e7a9e3ec584d8c499ea55c2257c16967cc
NoahAldhous <naldhous@hotmail.co.uk>
2022-05-04 00:11:42 +0100

    added goblin object

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
-
+//text assignment
 let header = document.querySelector("#header");
+let h2 = document.querySelector("#goblinattack")
 
+//buttons
 let button1 = document.querySelector("#sword")
 let button2 = document.querySelector("#axe")
 let button3 = document.querySelector("#dagger")
 
-let swordDamage = 6
-let axeDamage = 8
-let dagDamage = 4
 
-function changeHeader(event){
+//damage calculator
+let swordDamage = Math.floor(Math.random() * 6) + 1 + Math.floor(Math.random() * 6) + 1;
+//    console.log(swordDamage);
+let axeDamage = 2+ Math.floor(Math.random() * 12) + 1; 
+//    console.log(axeDamage);
+let dagDamage = Math.floor(Math.random() * 4) + 1 + Math.floor(Math.random() * 4) + 1;
+//    console.log(dagDamage);
+
+//function
+function chooseWeapon(event){
 header.innerText = `You have chosen the ${event.target.innerText}`;
 button1.remove();
 button2.remove();
 button3.remove();
 console.log(event.target.innerText);
+h2.innerText = `Goblins attack!`
 }
 
-button1.addEventListener("click", changeHeader);
-button2.addEventListener("click", changeHeader);
-button3.addEventListener("click", changeHeader);
+//clickable buttons
+button1.addEventListener("click", chooseWeapon);
+button2.addEventListener("click", chooseWeapon);
+button3.addEventListener("click", chooseWeapon);
 
+let Goblin = {
+    name : "goblin1",
+    hp : 25
+}
